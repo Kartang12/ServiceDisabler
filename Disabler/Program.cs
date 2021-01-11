581b48a155a3250fdf5b498c11479b87be8039f9
Kartang <vasiltsow.by@gmail.com>
2021-01-11 14:45:38 +0300

    multithread

--- Disabler/Program.cs
+++ Disabler/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Disabler
 {
@@ -17,19 +18,34 @@ namespace Disabler
 
             serviceNames = File.ReadAllLines("DisableList.txt").ToList();
 
+            
+
             foreach (var service in services)
                 if (serviceNames.Contains(service.ServiceName))
                 {
-                    if (service.CanStop)
-                    {
-                        Console.WriteLine(service.ServiceName + " - " + service.StartType);
-                        service.Stop();
-                        ServiceHelper.ChangeStartMode(service, ServiceStartMode.Disabled);
-                        //Thread.Sleep(3000);
-                    }
+                    //if (service.CanStop)
+                    //{
+                        Task.Run(() => StopTask(service));
+                    //}
                     Console.WriteLine("\t" + "name: " + service.ServiceName + " \tdisabled: " + service.Status + "\t startMode:" + service.StartType);
                 }
             Console.ReadLine();
+        }
+
+        static void StopTask(ServiceController service)
+        {
+
+            int worker = 0;
+
+            int available = 0;
+
+            ThreadPool.GetAvailableThreads(out worker, out available);
+
+            //Console.WriteLine(service.ServiceName + " - " + service.StartType);
+            //service.Stop();
+            //ServiceHelper.ChangeStartMode(service, ServiceStartMode.Disabled);
+            Console.WriteLine("worker: " + worker + " availdable:" + available);
+            Thread.Sleep(2000);
         }
     }
 }
