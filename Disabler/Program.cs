061e424337469400207a86de1b2b1375583ce823
Vlad Vasiltsov <vvasiltsov@elinext.com>
2022-06-04 17:10:06 +0300

    nowItWorks

--- Disabler/Program.cs
+++ Disabler/Program.cs
@@ -15,37 +15,46 @@ namespace Disabler
         {
             List<string> serviceNames = new List<string>();
             ServiceController[] services = ServiceController.GetServices();
-
-            serviceNames = File.ReadAllLines("DisableList.txt").ToList();
+            if (!File.Exists("ServicesList.txt"))
+                CreateServicesList();
+            try
+            {
+                serviceNames = File.ReadAllLines("DisableList.txt").ToList();
+            } catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Please create a list of services and put it to DisableList.txt each name from new line");
+            }
 
             
 
             foreach (var service in services)
                 if (serviceNames.Contains(service.ServiceName))
                 {
-                    //if (service.CanStop)
-                    //{
+                    if (service.CanStop)
                         Task.Run(() => StopTask(service));
-                    //}
+                    else
+                        Console.WriteLine("service" + service.ServiceName + "can't be stopped");
                     Console.WriteLine("\t" + "name: " + service.ServiceName + " \tdisabled: " + service.Status + "\t startMode:" + service.StartType);
                 }
             Console.ReadLine();
         }
 
-        static void StopTask(ServiceController service)
+        private static void StopTask(ServiceController service)
         {
+            service.Stop();
+            ServiceHelper.ChangeStartMode(service, ServiceStartMode.Disabled);
+        }
 
-            int worker = 0;
-
-            int available = 0;
-
-            ThreadPool.GetAvailableThreads(out worker, out available);
-
-            //Console.WriteLine(service.ServiceName + " - " + service.StartType);
-            //service.Stop();
-            //ServiceHelper.ChangeStartMode(service, ServiceStartMode.Disabled);
-            Console.WriteLine("worker: " + worker + " availdable:" + available);
-            Thread.Sleep(2000);
+        private static void CreateServicesList()
+        {
+            ServiceController[] services = ServiceController.GetServices();
+            using (StreamWriter sw = File.CreateText("ServicesList.txt"))
+            {
+                foreach(var service in services)
+                {
+                    sw.WriteLine(service.ServiceName + " - " + service.DisplayName);
+                }
+            }
         }
     }
 }
